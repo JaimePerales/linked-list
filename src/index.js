9d075807e17bf83dd34602fdccd0e7aa4faf5273
JaimePerales <jaimeperales123@gmail.com>
2023-03-01 23:57:37 -0600

    Implement more linked list functions

--- src/index.js
+++ src/index.js
@@ -5,8 +5,25 @@ list.append("Hello");
 list.append("World!");
 list.prepend('yo,');
 list.prepend('lets, go!');
+list.append('bingo');
+list.append(3);
+
+list.insertAt("Pizza", 3);
+list.insertAt("cube", 20);
 
 
 
-console.log(`The Size of the Linked List is: ${list.size()}`)
-list.print();
+
+
+// console.log(`The popped value is ${list.pop().value}`);
+// console.log('The new list is: ')
+// list.print();
+// console.log(`The popped value is ${list.pop().value}`);
+// console.log('The new list is: ')
+// list.print();
+console.log(list.contains("Hello", list.head));
+console.log(list.contains("bingos", list.head));
+
+console.log(list.find(3, list.head, 0));
+
+console.log(list.toString());
--- src/linkedList.js
+++ src/linkedList.js
@@ -1,9 +1,7 @@
 import Node from "./node";
 
 class LinkedList {
-    head;
 
-    tail;
 
     constructor() {
         this.head = null;
@@ -50,16 +48,113 @@ class LinkedList {
 
     }
 
-    print() {
+    head() {
+        return this.head;
+    }
+
+    tail() {
+        return this.tail;
+    }
+
+    at(index) {
+        let currentNode = this.head;
+        let i = 0;
+        while (currentNode !== null && i !== index) {
+            currentNode = currentNode.nextNode;
+            i += 1;
+        }
+
+        return currentNode;
+    }
+
+    pop() {
+        const poppedElement = this.tail;
+        let currentNode = this.head;
+        while (currentNode.nextNode !== this.tail) {
+            currentNode = currentNode.nextNode;
+        }
+        this.tail = currentNode;
+        this.tail.nextNode = null;
+        return poppedElement;
+    }
+
+    // Solved with Recurrion
+    // check if first element contains value, else run method starting at next node;
+    // if next null is null, list does not contatin value
+
+    contains(value, node) {
+        if (node === null) {
+            return false
+        }
+        if (node.value === value) {
+            return true;
+        }
+
+
+        return this.contains(value, node.nextNode);
+
+
+    }
+
+    find(value, node, index) {
+        if (node === null) {
+            return false
+        }
+        if (node.value === value) {
+            return index;
+        }
+
+        let count = index;
+        count += 1;
+
+        return this.find(value, node.nextNode, count);
+
+
+    }
+
+    insertAt(value, index) {
+
+        // If placing value at beggining of list runs prepend to update head
+        if (index === 0) {
+            this.prepend(value);
+            return;
+        }
+
+        // Gets the node pointing to the node at the provided index
+        const nodeBeforeIndex = this.at(index - 1);
+
+        // Getes the node at the index
+        const nodeAtIndex = this.at(index);
+
+        // If the index is greater than the size of the list, append value
+        if (nodeAtIndex === null) {
+            this.append(value);
+            return;
+        }
+        // Creates new node
+        const newNode = new Node;
+        newNode.value = value;
+
+        // Updates node before index to point at new node
+        nodeBeforeIndex.nextNode = newNode;
+
+        // Updates node to point at previous node at index
+        newNode.nextNode = nodeAtIndex;
+    }
+
+    toString() {
         let node = this.head;
         if (node === null) {
-            console.log("Empty List");
-            return
+
+            return "Empty List"
         }
+        let returnString = '';
         while (node !== null) {
-            console.log(node.value);
+            returnString += `( ${node.value} ) -> `;
             node = node.nextNode;
         }
+        returnString += 'null';
+        return returnString;
     }
 
 }
